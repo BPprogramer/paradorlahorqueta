b44a4ebccf5f9617d344e9c5b9d2159c15525d05
BPprogramer <piambabryan20@gmail.com>
2023-05-10 19:23:44 -0500

    actualización del titulo

--- views/plantilla.php
+++ views/plantilla.php
@@ -22,7 +22,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-  <title>Inventory System</title>
+  <title>La Horqueta</title>
 
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
